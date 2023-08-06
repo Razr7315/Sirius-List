ba374050925b20bf4d67191ba1d7f56623497db1
Razr <92069712+Razr7315@users.noreply.github.com>
2023-08-05 21:32:45 -0400

    Update load.lua

--- load/load.lua
+++ load/load.lua
@@ -2,15 +2,15 @@
 Commented out until the new games release.
 
 local ids = {
-    142823291, -- MM2
-    286090429, -- Arsenal
-    1962086868, -- ToH
-    292439477, -- PF
-    3233893879, -- BB
     6516141723, -- Doors
-    5938036553, -- FRONTLINES
-    443406476, -- Lazarus
-    3527629287 -- BP
+    292439477, -- Phantom Forces
+    648362523, -- Breaking Point
+    2377868063, -- Strucid
+    443406476, -- Project Lazarus
+    2916899287, -- BHRM5
+    286090429, -- Arsenal
+    142823291, -- MM2
+    1962086868 - ToH
 }
 
 if table.find(ids, game.PlaceId) then
